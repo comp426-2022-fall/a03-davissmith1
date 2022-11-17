12ddb2ed28beb34fa674b4101ba5bfd14eced4c4
Davis Smith <44070645+davissmith1@users.noreply.github.com>
2022-11-16 19:21:02 -0500

    working on fixes

--- bin/cli.js
+++ bin/cli.js
@@ -1,8 +1,8 @@
 import { roll } from "/lib/roll.js"
 
 //minimist import and parsing 
-const minimist = require("minimist")
-const args = minimist(process.argv.slice(2))
+import minimist from 'minimist';
+const args = minimist(process.argv.slice(2));
 
 //variables
 const sides = args.sides;
@@ -10,3 +10,5 @@ const dice = args.dice
 const rolls = args.rolls
 
 const output = JSON.stringify(roll(sides, dice, rolls))
+
+return(output);
